13c941b568bd8c5300fc830c967590d92aad194b
soezyxstt <16922204@mahasiswa.itb.ac.id>
2025-02-11 23:05:58 +0700

    make help button fixed

--- src/app/help-button.tsx
+++ src/app/help-button.tsx
@@ -13,7 +13,7 @@ export default function HelpButton() {
     setExpanded(false)
   })
   return (
-    <motion.button ref={ref} onClick={() => setExpanded(prev => !prev)} layout className='px-4 py-2 absolute bottom-6 right-4 md:bottom-8 md:right-6 z-50 gap-3 backdrop-blur-sm bg-white/30 border border-white text-clear-sky-m font-medium overflow-hidden'
+    <motion.button ref={ref} onClick={() => setExpanded(prev => !prev)} layout className='px-4 py-2 fixed bottom-6 right-4 md:bottom-8 md:right-6 z-50 gap-3 backdrop-blur-sm bg-white/30 border border-white text-clear-sky-m font-medium overflow-hidden'
       style={{
         borderRadius: expanded ? 16 : 9999
       }}
